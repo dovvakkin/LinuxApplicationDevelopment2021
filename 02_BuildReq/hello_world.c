60ee50b170ee18dd9dbe3b00fbc96f1a1867a566
y.kozhevnikov <y.kozhevnikov@ykozhevnikov-dev0.openstack.go.devmail.ru>
2021-09-22 00:57:32 +0300

    add 02_BuildReq (hello world)

---
+++ 02_BuildReq/hello_world.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void print_hello_world()
+{
+  printf("Hello, World!\n");
+}
+
+int main()
+{
+  print_hello_world();
+  return 0;
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+Repo for https://uneex.org/LecturesCMC/LinuxApplicationDevelopment2021 course homeworks
+
+* 02\_BuildReq -- hello world in C
